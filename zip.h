5fbb81979004b196379fd86f47501b06abe8344b
Ingo Oppermann <ingo@oppermann.ch>
2009-11-08 15:22:21 +0100

    Added the #define from zipstream.h and some missing prototypes

--- zip.h
+++ zip.h
@@ -3,16 +3,29 @@
 
 #include "zipstream.h"
 
-void zs_build_lfheader(ZSFile *zsf);
-void zs_build_descriptor(ZSFile *zsf);
+#define ZS_SIGNATURE_LFH	0x04034b50
+#define ZS_SIGNATURE_LFD	0x08074b50
+#define ZS_SIGNATURE_CDH	0x02014b50
+#define ZS_SIGNATURE_EOCD	0x06054b50
+
+#define ZS_LENGTH_LFH		30
+#define ZS_LENGTH_LFD		16
+#define ZS_LENGTH_CDH		46
+#define ZS_LENGTH_EOCD		22
+
+void zs_build_lfh(ZSFile *zsf);
+void zs_build_lfd(ZSFile *zsf);
+void zs_build_cdh(ZSFile *zsf);
+void zs_build_eocd(ZS *zs);
+
 void zs_build_offsets(ZSDirectory *zs);
-void zs_build_eocd(ZSDirectory *zs);
-void zs_build_cdheader(ZSFile *zsf);
 
-int zs_write_directory(ZSDirectory *zs, char *buf, int sbuf);
+int zs_write_stagedata(ZS *zs, char *buf, int sbuf, int size);
+int zs_write_filename(ZS *zs, char *buf, int sbuf);
+int zs_write_filedata(ZS *zs, char *buf, int sbuf);
 
-size_t zs_get_cdoffset(ZSDirectory *zs);
-size_t zs_get_cdsize(ZSDirectory *zs);
+size_t zs_get_cdoffset(ZS *zs);
+size_t zs_get_cdsize(ZS *zs);
 
 void zs_stager(ZS *zs);
 
